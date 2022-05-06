fb1d8a2c973bb422a73a96ce440b9d9d56dbe26c
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-05-06 14:51:30 +0500

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Bekhruz Hakmirzaev on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
